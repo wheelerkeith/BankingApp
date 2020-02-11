b6a599d3304b580a5bcc79ae3df60558d1d6a9b8
wheelerkeith <nemimimonkey@gmail.com>
2020-02-11 10:20:34 -0600

    Initial commit

---
+++ src/main/java/com/revature/project1/BankingApp/App.java
@@ -0,0 +1,13 @@
+package com.revature.project1.BankingApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
